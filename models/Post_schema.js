9e30b173f68ceee34faf1714c39f66804a4f3427
AKASH <akash.arthanur26@gmail.com>
2022-12-13 16:03:45 +0530

    api working

--- models/Post_schema.js
+++ models/Post_schema.js
@@ -3,12 +3,12 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const Post_Schema = new Schema({
-  name: { type: String, required: true },
-  location: { type: String, required: true },
-  likes: { type: Number, required: true },
-  description: { type: String, required: true },
-  PostImage: { type: String, required: true },
-  date: { type: String, required: true },
+    name: { type: String, required: true },
+    location: { type: String, required: true },
+    likes: { type: Number, required: true },
+    description: { type: String, required: true },
+    PostImage: { type: String, required: true },
+    date: { type: String, required: true },
 });
 const PostDB = mongoose.model("PostDB", Post_Schema);
 
--- routes/Post_router.js
+++ routes/Post_router.js
@@ -4,55 +4,73 @@ const mongoose = require("mongoose");
 const PostDB = require("../models/Post_schema");
 const Post_route = express.Router();
 const cloudinary = require("cloudinary").v2;
-
+const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+];
 cloudinary.config({
-  cloud_name: "dlzsf2clj",
-  api_key: "842877512291622",
-  api_secret: "HYYS9PEPtfprLLIj3sE3wcAQ5EA",
+    cloud_name: "dlzsf2clj",
+    api_key: "842877512291622",
+    api_secret: "HYYS9PEPtfprLLIj3sE3wcAQ5EA",
 });
 Post_route.get("/instaclone/data", async (req, res) => {
-  try {
-    const Posts = await PostDB.find();
-    res.status(200).json({
-      status: "Sucess",
-      Posts: Posts,
-    });
-  } catch (e) {
-    res.status(400).json({
-      status: "Failed to fetch",
-      message: e.message,
-    });
-  }
+    try {
+        const Posts = await PostDB.find();
+        res.status(200).json({
+            status: "Sucess",
+            Posts: Posts,
+        });
+    } catch (e) {
+        res.status(400).json({
+            status: "Failed to fetch",
+            message: e.message,
+        });
+    }
 });
 Post_route.post("/instaclone/data", async (req, res) => {
-  try {
-    const file = req.files.image;
-    const result = await cloudinary.uploader.upload(file.tempFilePath, {
-      public_id: `${Date.now()}`,
-      resource_type: "auto",
-      folder: "images",
-    });
-    const t = new Date();
-    const today =
-      t.getDate() + "-" + parseInt(t.getMonth()) + 1 + "-" + t.getFullYear();
-    const Posts = await PostDB.create({
-      name: req.body.name,
-      location: req.body.location,
-      likes: req.body.likes,
-      description: req.body.description,
-      PostImage: result.url,
-      date: today,
-    });
-    res.status(200).json({
-      status: "Sucess",
-      Posts: Posts,
-    });
-  } catch (e) {
-    res.status(400).json({
-      status: "Failed to post",
-      message: e.message,
-    });
-  }
+    try {
+        const file = req.files.image;
+        const result = await cloudinary.uploader.upload(file.tempFilePath, {
+            public_id: `${Date.now()}`,
+            resource_type: "auto",
+            folder: "images",
+        });
+
+        const t = new Date();
+        const today =
+            t.getDate() +
+            "-" +
+            months[parseInt(t.getMonth())] +
+            "-" +
+            t.getFullYear();
+        const Posts = await PostDB.create({
+            name: req.body.name,
+            location: req.body.location,
+            likes: Math.floor(Math.random() * 1000),
+            description: req.body.description,
+            PostImage: result.url,
+            date: today,
+        });
+        res.status(200).json({
+            status: "Sucess",
+            Posts: Posts,
+        });
+    } catch (e) {
+        res.status(400).json({
+            status: "Failed to post",
+            message: e.message,
+        });
+    }
 });
 
 module.exports = Post_route;
