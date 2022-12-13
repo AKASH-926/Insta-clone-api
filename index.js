97499e3f8bac877231f75511757c76741eecca42
AKASH <akash.arthanur26@gmail.com>
2022-12-13 16:36:30 +0530

    port config chenaged

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = require("./app");
-
+const port = process.env.PORT || 8000;
 dotenv.config();
 mongoose.connect(
   process.env.DATABASE_URL,
@@ -14,4 +14,4 @@ mongoose.connect(
   }
 );
 
-app.listen(8000, () => console.log("Server running at 8000"));
+app.listen(port, () => console.log(`sever up at ${port}`));
